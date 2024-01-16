a8e982550e8a6c86717c3e37fe25d97a5bcb8b6e
Francesco-Rapetti <rapettif80@yahoo.it>
2024-01-16 16:46:09 +0100

    Bugfix: generates html based on category's products

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
     require __DIR__ . "/data.php";
+    foreach ($categories as $category) {
+        $category->setProducts(array_filter($products, fn(Product $product) => $product->category->name === $category->name));
+    }
 ?>
 
 <!DOCTYPE html>
@@ -11,32 +14,37 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container" style="background-image: url('<?= $animalsProduct->image ?>');">
-        <div class="row gap-3">
-            <div class="glass w-auto">
-                <span class="fs-1 fw-bold"><?= $animalsProduct->name ?></span>
-            </div>
+    <?php foreach ($categories as $category) : ?>
+    <div class="container" style="background-image: url('<?= $category->image ?>');">
+        <div class="row">
 
-            <!-- spacer -->
-            <div></div>
+                <div class="glass w-auto pb-2 m-2">
+                    <div class="fs-1 fw-bold"><?= $category->name ?></div>
+                    <span><?= $category->description ?></span>
+                </div>
 
-            <?php foreach ($products as $product) : ?>
-                <div class="card glass" style="width: 18rem;">
-                    <div class="card-img-top-container d-flex align-items-center justify-content-center">
-                        <img src="<?= $product->image ?>" class="card-img-top" alt="...">
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $product->name ?></h5>
-                        <p class="card-text"><?= $product->description ?></p>
-                        <div class="d-flex align-items-center mt-3">
-                            <a href="#" class="btn btn-primary me-3">Acquista</a>
-                            <p class="fw-bold fs-5"><?= $product->price ?> €</p>
+                <!-- spacer -->
+                <div class="spacer"></div>
+
+
+                <?php foreach ($category->products as $product) : ?>
+                    <div class="card m-2 glass" style="width: 18rem;">
+                        <div class="card-img-top-container d-flex align-items-center justify-content-center">
+                            <img src="<?= $product->image ?>" class="card-img-top" alt="...">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $product->name ?></h5>
+                            <p class="card-text"><?= $product->description ?></p>
+                            <div class="d-flex align-items-center mt-3">
+                                <a href="#" class="btn btn-primary me-3">Acquista</a>
+                                <p class="fw-bold fs-5"><?= $product->price ?> €</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            <?php endforeach; ?>
+                <?php endforeach; ?>
+            </div>
         </div>
-    </div>
+    <?php endforeach; ?>
 </body>
 </html>
 
@@ -52,6 +60,12 @@
         padding: 1rem 2rem;
         margin: 50px auto;
         overflow: hidden;
+    }
+
+    .spacer {
+        height: 0;
+        margin: 0;
+        padding: 0;
     }
 
     .glass {
