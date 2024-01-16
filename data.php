cc2fad742ee0ecdeedd0c790b22f1d4cee26ce5e
Francesco-Rapetti <rapettif80@yahoo.it>
2024-01-16 19:51:45 +0100

    Feat: implements chart item counter

--- data.php
+++ data.php
@@ -1,6 +1,8 @@
 <?php
 
-require __DIR__ ."/Models/Product.php";
+require __DIR__ . "/Models/Product.php";
+
+$cartCounter = 0;
 
 $categories = [
     $food = new Category(
@@ -31,32 +33,32 @@ $categories = [
 $products = [
     new Product(
         $food,
-        'https://www.giramico.it/wp-content/uploads/2014/06/HP-MINI-pesce-mockup-sito-nobackground.png', 
-        'Crocchette per cani', 
+        'https://www.giramico.it/wp-content/uploads/2014/06/HP-MINI-pesce-mockup-sito-nobackground.png',
+        'Crocchette per cani',
         'Crocchette per cani molto buone. Per 10 kg di cane.',
         10.99
     ),
-    
+
     new Product(
         $food,
-        'https://static.wixstatic.com/media/40eb25_4be89889e63f49899783ed9af86a3750~mv2.png/v1/fit/w_500,h_500,q_90/file.png', 
-        'Mangime per tartarughe', 
+        'https://static.wixstatic.com/media/40eb25_4be89889e63f49899783ed9af86a3750~mv2.png/v1/fit/w_500,h_500,q_90/file.png',
+        'Mangime per tartarughe',
         'Mangime per tartarughe di acqua dolce. Contiene 250g di prodotto.',
         4.99
     ),
 
     new Product(
         $food,
-        'https://www.kaytee.com/-/media/Project/OneWeb/Kaytee/US/all-products/wild-bird/woodlands-wild-bird/10lb/03013_Woodlands_10lb-png.png', 
-        'Mangime per uccelli', 
+        'https://www.kaytee.com/-/media/Project/OneWeb/Kaytee/US/all-products/wild-bird/woodlands-wild-bird/10lb/03013_Woodlands_10lb-png.png',
+        'Mangime per uccelli',
         'Mangime adatto a uccelli di piccole dimensioni. Contiene 250g di prodotto.',
         5.99
     ),
 
     new Product(
         $food,
-        'https://revealpetfood.com/wp-content/uploads/2023/01/1.-Optimised-Image-Transparent-Background-e1677118541269.png', 
-        'Crocchette per gatti', 
+        'https://revealpetfood.com/wp-content/uploads/2023/01/1.-Optimised-Image-Transparent-Background-e1677118541269.png',
+        'Crocchette per gatti',
         'Crocchette per gatti al gusto di pollo. Per gatti fino a 10 kg.',
         8.99
     ),
