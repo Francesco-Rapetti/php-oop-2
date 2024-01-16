de3e0a43e5cc4045bc9d253ab7a164d3152986ce
Francesco-Rapetti <rapettif80@yahoo.it>
2024-01-16 18:22:34 +0100

    Change category type

--- data.php
+++ data.php
@@ -3,22 +3,22 @@
 require __DIR__ ."/Models/Product.php";
 
 $categories = [
-    $animalsProduct = new Category(
-        "Prodotti per animali",
+    $food = new Category(
+        "Cibo",
         "https://static.vecteezy.com/system/resources/previews/025/268/369/non_2x/dog-food-seamless-pattern-bones-food-bowls-pets-background-for-pet-shop-websites-and-prints-social-media-posts-animal-product-design-cartoon-illustration-vector.jpg",
-        "Prodotti per la cura degli animali domestici."
+        "Cibo per tutti gli animali domestici"
     ),
 
-    $homeProduct = new Category(
-        "Prodotti per la casa",
-        "https://iconspng.com/uploads/kitchenutensilscrockeryseamlesspattern/kitchenutensilscrockeryseamlesspattern.png",
-        "Prodotti per la cura della casa."
+    $accessories = new Category(
+        "Accessori",
+        "https://previews.123rf.com/images/amalga/amalga1502/amalga150200047/36801893-seamless-pattern-con-accessori-per-cani-vector-cartoon-sfondo.jpg",
+        "Accessori per tutti gli animali domestici."
     )
 ];
 
 $products = [
     new Product(
-        $animalsProduct,
+        $food,
         'https://www.giramico.it/wp-content/uploads/2014/06/HP-MINI-pesce-mockup-sito-nobackground.png', 
         'Crocchette per cani', 
         'Crocchette per cani molto buone. Per 10 kg di cane.',
@@ -26,7 +26,7 @@ $products = [
     ),
     
     new Product(
-        $animalsProduct,
+        $food,
         'https://static.wixstatic.com/media/40eb25_4be89889e63f49899783ed9af86a3750~mv2.png/v1/fit/w_500,h_500,q_90/file.png', 
         'Mangime per tartarughe', 
         'Mangime per tartarughe di acqua dolce. Contiene 250g di prodotto.',
@@ -34,50 +34,50 @@ $products = [
     ),
 
     new Product(
-        $animalsProduct,
-        'https://cdn.reddingo.com/assets/product/overview/CC-DC-PK.png', 
-        'Collare per gatti', 
-        'Collare per gatti resistente in microfibra. Dotato di campanellino e targhetta.',
-        14.99
+        $food,
+        'https://www.kaytee.com/-/media/Project/OneWeb/Kaytee/US/all-products/wild-bird/woodlands-wild-bird/10lb/03013_Woodlands_10lb-png.png', 
+        'Mangime per uccelli', 
+        'Mangime adatto a uccelli di piccole dimensioni. Contiene 250g di prodotto.',
+        5.99
     ),
 
     new Product(
-        $animalsProduct,
-        'https://www.morsoworld.com/wp-content/uploads/2023/03/pettorina_mini-regolabile_lipstick_header_01.png', 
-        'Pettorina', 
-        'Pettorina in materiale elastico. Per cani e gatti.',
-        17.99
+        $food,
+        'https://revealpetfood.com/wp-content/uploads/2023/01/1.-Optimised-Image-Transparent-Background-e1677118541269.png', 
+        'Crocchette per gatti', 
+        'Crocchette per gatti al gusto di pollo. Per gatti fino a 10 kg.',
+        8.99
     ),
 
     new Product(
-        $homeProduct,
-        'https://content.dambros.it/uploads/2023/03/20093818/0000217782.png',
-        'Detersivo per piatti',
+        $accessories,
+        'https://cdn11.bigcommerce.com/s-ucjiusw7rc/images/stencil/original/products/233/466/47143_BonoFido_SoftCrate_Front__79892.1563279285.png?c=2',
+        'Cuccia per cani',
         'Detersivo per piatti al gusto limone. Sgrassante e igienizzante.',
         2.99
     ),
 
     new Product(
-        $homeProduct,
-        'https://www.sagigroup.com/wp-content/uploads/2020/02/a75-015dcdeomatic_notedipulito_diff.png',
-        'Deodorante per ambienti',
+        $accessories,
+        'https://www.arkvetofliberty.com/wp-content/uploads/2017/09/litter-box-solutions.png',
+        'Lettiera per gatti',
         'Deodorante per ambienti al gurto di cannella. Perfetto per la notte.',
         9.99
     ),
 
     new Product(
-        $homeProduct,
-        'https://www.midea.com/content/dam/midea-aem/my/vacuum-cleaners/robotics/4000pa--mvc-s8-/Midae_S8_EN_1500X1500_02.png',
-        'Robot aspirapolvere',
-        'Robot aspirapolvere in grado di pulire fino a 250 metri quadri. Dotato di connessione wifi',
-        349.99
+        $accessories,
+        'https://www.freeiconspng.com/uploads/red-leather-dog-collar-belt-images-1.png',
+        'Collare',
+        'Collare per gatti e cani di piccola taglia in microfibra.',
+        9.99
     ),
 
     new Product(
-        $homeProduct,
-        'https://www.detergenzaprofessionaleonline.it/wp-content/uploads/R46-GUANTO.png',
-        'Guanti in lattice',
-        'Guanti in lattice per l\'uso casalingo. Confezione da 50pz',
-        3.99
+        $accessories,
+        'https://pawshtails.com/wp-content/uploads/2022/05/red-harn2.png',
+        'Pettorina',
+        'Pettorina per gatti e cani di piccola taglia in microfibra.',
+        11.99
     )
 ];
