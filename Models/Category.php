0eb6472fd1c174a2b0ec77a0ee610bbea749a413
Francesco-Rapetti <rapettif80@yahoo.it>
2024-01-16 16:46:32 +0100

    Feat: implements products as category's field

--- Models/Category.php
+++ Models/Category.php
@@ -4,9 +4,18 @@ class Category {
     public $name;
     public $image;
     public $description;
-    public function __construct($name, $image, $description) {
+    public $products;
+    public function __construct($name, $image, $description, array $products = []) {
         $this->name = $name;
         $this->image = $image;
         $this->description = $description;
+    }
+
+    public function setProducts($products) {
+        $this->products = $products;
+    }
+
+    public function addProduct($product) {
+        $this->products[] = $product;
     }
 }
